c3f0e9393072ba6fe0418ddbe40a45e0651d294e
kos <ustyugovki@bk.ru>
2023-01-13 00:50:29 +0300

    Декомпозиция на компоненты. Создал пустые заглушки для компонентов.

---
+++ src/components/ui/product-card/product-card.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./product-card.css";
+
+function ProductCard() {
+  return <></>;
+}
+
+export default ProductCard;
---
+++ src/components/ui/title/title.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./title.css";
+
+function Title() {
+  return <></>;
+}
+
+export default Title;
