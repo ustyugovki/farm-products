4fac1c4718788f5dd23bb3af9a32490f95de79fb
kos <ustyugovki@bk.ru>
2023-01-14 11:25:25 +0300

    Сверстал Footer.

--- src/components/layout/footer/footer.jsx
+++ src/components/layout/footer/footer.jsx
@@ -1,9 +1,15 @@
 import React from "react";
 import "./footer.css";
 
+import Logo from '../../ui/logo/logo';
+
 function Footer() {
   return (
     <footer className='footer'>
+      <div className='footer__wrapper'>
+        <Logo />
+        <span className='footer_сopyright'>Создано 2021</span>
+      </div>
     </footer>
   );
 }
