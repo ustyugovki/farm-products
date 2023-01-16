9af4f0c92c2577b14bb1961270952e249407710d
kos <ustyugovki@bk.ru>
2023-01-16 09:09:10 +0300

    Добавил mock

--- src/index.js
+++ src/index.js
@@ -2,11 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './components/app/App';
+import { productList } from './mocks/mock';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+console.log(productList.features);
 root.render(
   <React.StrictMode>
-    <App />
+    <App features={productList.features} />
   </React.StrictMode>
 );
-
---
+++ src/mocks/mock.js
@@ -0,0 +1,36 @@
+export const productList = {
+  features: [
+    {
+      id: 0,
+      name: "Еда намного вкуснее",
+      about:
+        "Домашняя колбаса из мяса, соли и специй и колбаса из магазина — два настолько разных продукта, что они даже не родственники",
+      image: "../assets/featureLogo1.svg",
+      feature: "feature__owner"
+    },
+    {
+      id: 1,
+      name: "Просроченные продукты",
+      about:
+        "Из-за большого количество товара сотрудники магазинов не успевают своевременно производить замену товара",
+      image: "../assets/featureLogo2.svg",
+      feature: "feature__owner_negative"
+    },
+    {
+      id: 2,
+      name: "Натуральные продукты",
+      about:
+        "Поставляем местные органические продукты выращенные без пестицидов и химических удобрений",
+      image: "../assets/featureLogo3.svg",
+      feature: "feature__owner"
+    },
+    {
+      id: 3,
+      name: "Некачественное мясо",
+      about:
+        "Мясные полуфабрикаты, в которых содержится чрезмерно много натрия, вредных жиров, консервантов",
+      image: "../assets/featureLogo4.svg",
+      feature: "feature__owner_negative"
+    }
+  ]
+};
